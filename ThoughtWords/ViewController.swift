b43885fbd19e3540ad4eb1c101397ff3c54a9601
SangMee Specht <spechtse@miamioh.edu>
2017-07-26 11:09:49 -0500

    [SangMee] intial commit

---
+++ ThoughtWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThoughtWords
+//
+//  Created by SangMee Specht on 7/26/17.
+//  Copyright © 2017 SangMee Specht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
